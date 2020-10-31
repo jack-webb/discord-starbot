d109d74682003123d0b4943bb38f9920bc3ef744
Jack Webb <git@jackwebb.me>
2020-10-31 12:35:28 +0000

    Update default embed colour

--- config.example.py
+++ config.example.py
@@ -1,6 +1,6 @@
 token = "your-token"  # Your bot token
 emoji = "\U00002b50"  # Reaction to listen for (unicode encoding)
-colour = (0, 0, 0)  # RGB, highlight colour of embeds
+colour = (249, 166, 2)  # RGB, highlight colour of embeds
 star_channel = 0  # The ID of the channel the bot posts stars to
 threshold = 1  # The number of stars required to appear in the star channel
 
